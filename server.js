04f7a08f32c2b1bc79884c76ee1184d8ba5bd124
unknown <tobytw312@gmail.com>
2023-10-19 14:08:09 +0100

    working auto html, next styles, and try to implement auto scrolling when idle

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const jsonFile = "data.json";
 const htmlFile = "index.html";
 const csvFile = "data.csv";
 const excelFile = "data.xlsx";
+const mediaFoldersFolder = "media";
 
 app.use(express.static(path.join(__dirname)));
 
@@ -33,15 +34,89 @@ function removeEmptyFields(obj) {
 }
 
 
+
+
+function makeMedia(folderName){
+    try {
+        const files = fs.readdirSync(mediaFoldersFolder+"/"+folderName);
+        console.log("success reading media folder");
+        if (files.length < 1){
+            console.log("folder empty");
+            return null;
+        }else if (files.length == 1){
+            console.log("folder "+folderName+" has one file");
+            console.log(files);
+            return(mediaFoldersFolder+"/"+folderName+"/"+files[0]);
+        }else{
+            // folder has multiple media. make appropriate html
+            console.log("MultiMedia Maker");
+            console.log(files);
+            console.log("loading multi ");
+            
+
+            // Create an empty string to store the HTML elements
+            let htmlContent = `<!DOCTYPE html><html><body>\n`;
+
+            // Loop through the list of media files and add the appropriate HTML for each
+            files.forEach(file => {
+                const fileExtension = path.extname(file);
+                if (fileExtension.match(/\.(jpg|jpeg|png|gif)$/i)) {
+                    // Images
+                    htmlContent += `\n<img src="${folderName}/${file}">\n`;
+                } else if (fileExtension === '.mp3') {
+                    // Audio
+                    htmlContent += `\n<audio controls>\n<source src="${folderName}/${file}" type="audio/mpeg">\nYour browser does not support the audio element.\n</audio>\n`;
+                } else if (fileExtension === '.mp4') {
+                    // Video
+                    htmlContent += `\n<video width="640" height="360" controls>\n<source src="${folderName}/${file}" type="video/mp4">\nYour browser does not support the video tag.\n</video>\n`;
+                }
+            });
+
+            // Close the HTML body and document
+            htmlContent += `\n</body></html>`;
+
+            // Save the HTML content to a file in a folder (e.g., "generated")
+            fs.writeFileSync('./'+mediaFoldersFolder+'/'+folderName+'.html', htmlContent);
+
+            return("<iframe src = "+mediaFoldersFolder+"/"+folderName+".html width = 100% height = 100% frameborder=0 style=border: none draggable=true></iframe>")
+
+
+
+
+            // gotta return something like this 
+            // "<iframe src = testiframe.html width = 100% height = 100% frameborder=0 style=border: none draggable=true></iframe>",
+        }
+
+    } catch (error) {
+        console.error(`Error reading directory: ${error}`);
+
+    }
+}
+
+function deleteHTMLFilesInFolder(folderName) {
+    fs.readdirSync(folderName).forEach(file => {
+        const filePath = path.join(folderName, file);
+        if (path.extname(file) === '.html') {
+            fs.unlinkSync(filePath);
+            console.log(`Deleted file: ${file}`);
+        }
+    });
+}
+
+
 app.get('/data', async (req, res) => {
 
     //create the json as a js object
     console.log('creating json from excel');
 
+
     try {
         const workbook = new ExcelJS.Workbook();
         await workbook.xlsx.readFile(excelFile);
         const worksheet = workbook.getWorksheet(1);
+
+        //delete old html files from mediaFoldersFolder
+        deleteHTMLFilesInFolder(mediaFoldersFolder);
 
         
     
@@ -53,6 +128,7 @@ app.get('/data', async (req, res) => {
 
 
             if (rowNum == 2){
+                // for title page
                 const rowData = {};
                 row.eachCell((cell, colNum) => {
                     rowData[worksheet.getRow(1).getCell(colNum).value] = cell.value;
@@ -62,7 +138,7 @@ app.get('/data', async (req, res) => {
                         ...(rowData['Media'] ? {
                             "media": 
                                 {
-                                "url": "media/"+rowData['Media'],
+                                "url": makeMedia(rowData['Media']),
                                 "caption": rowData['Media_Caption'],
                                 "credit": rowData['Media_Credit']
                                 }
@@ -87,18 +163,20 @@ app.get('/data', async (req, res) => {
                     ...(rowData['Media'] ? {
                         "media": 
                             {
-                            "url": "media/"+rowData['Media'],
+                            "url": makeMedia(rowData['Media']),
                             "caption": rowData['Media_Caption'],
                             "credit": rowData['Media_Credit']
                             }
-                    } : {
-                        "media": 
+                    } : {})
+
+                    /* FOR MULTIMEDIA
+                    "media": 
                             {
                             "url": "<iframe src = testiframe.html width = 100% height = 100% frameborder=0 style=border: none draggable=true></iframe>",
                             "caption": rowData['Media_Caption'],
                             "credit": rowData['Media_Credit']
                             }
-                    })
+                    */
                     
                     ,
                     "start_date": {
@@ -121,7 +199,7 @@ app.get('/data', async (req, res) => {
                     },
                     "background": {
                         "color": rowData['Background_hex'],
-                        "url": "media/"+rowData['Background_url']
+                        "url": makeMedia(rowData['Background_url'])
                     },
                 });
             }
