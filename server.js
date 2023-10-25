9bedb763e855fdc2345a119b35463762f14017a6
unknown <tobytw312@gmail.com>
2023-10-25 13:34:19 +0100

    formatting and tidying

--- server.js
+++ server.js
@@ -2,21 +2,20 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const csvParser = require('csv-parser');
 const ExcelJS = require('exceljs');
 
 const app = express();
 const server = http.createServer(app);
 
 const PORT = 3000;
-const jsonFile = "data.json";
-const htmlFile = "index.html";
-const csvFile = "data.csv";
+
+// DATA LOCATIONS 
 const excelFile = "data.xlsx";
 const mediaFoldersFolder = "media";
 
 app.use(express.static(path.join(__dirname)));
 
+//Function to filter json for unused fields
 function removeEmptyFields(obj) {
     for (const key in obj) {
         if (obj[key] === null || obj[key] === undefined || obj[key] === '') {
@@ -33,102 +32,70 @@ function removeEmptyFields(obj) {
     return obj;
 }
 
-
-
-
+// Function to create html from folders of media
 function makeMedia(folderName){
-    try {
-        const files = fs.readdirSync(mediaFoldersFolder+"/"+folderName);
-        console.log("success reading media folder");
-        if (files.length < 1){
-            console.log("folder empty");
-            return null;
-        }else if (files.length == 1){
-            console.log("folder "+folderName+" has one file");
-            console.log(files);
-            return(mediaFoldersFolder+"/"+folderName+"/"+files[0]);
-        }else{
-            // folder has multiple media. make appropriate html
-            console.log("MultiMedia Maker");
-            console.log(files);
-            console.log("loading multi ");
-            
+    if (folderName){
+        try {
+            const files = fs.readdirSync(mediaFoldersFolder+"/"+folderName);
+            if (files.length < 1){
+                console.log("Nothing detected in folder "+folderName);
+                return null;
+            }else if (files.length == 1){
+                return(mediaFoldersFolder+"/"+folderName+"/"+files[0]);
+            }else{
+                // Empty HTML document string
+                let htmlContent = `<!DOCTYPE html><html><body>\n`;
 
-            // Create an empty string to store the HTML elements
-            let htmlContent = `<!DOCTYPE html><html><body>\n`;
+                // Loop through files adding the appropriate HTML
+                files.forEach(file => {
+                    const fileExtension = path.extname(file);
+                    if (fileExtension.match(/\.(jpg|jpeg|png|gif)$/i)) {
+                        // Images
+                        htmlContent += `\n<img src="${folderName}/${file}" style="width: 100%" >\n`;
+                    } else if (fileExtension === '.mp3') {
+                        // Audio
+                        htmlContent += `\n<audio controls>\n<source src="${folderName}/${file}" type="audio/mpeg">\nYour browser does not support the audio element.\n</audio>\n`;
+                    } else if (fileExtension === '.mp4') {
+                        // Video
+                        htmlContent += `\n<video width="640" height="360" controls>\n<source src="${folderName}/${file}" type="video/mp4" >\nYour browser does not support the video tag.\n</video>\n`;
+                    }
+                });
 
-            // Loop through the list of media files and add the appropriate HTML for each
-            files.forEach(file => {
-                const fileExtension = path.extname(file);
-                if (fileExtension.match(/\.(jpg|jpeg|png|gif)$/i)) {
-                    // Images
-                    htmlContent += `\n<img src="${folderName}/${file}">\n`;
-                } else if (fileExtension === '.mp3') {
-                    // Audio
-                    htmlContent += `\n<audio controls>\n<source src="${folderName}/${file}" type="audio/mpeg">\nYour browser does not support the audio element.\n</audio>\n`;
-                } else if (fileExtension === '.mp4') {
-                    // Video
-                    htmlContent += `\n<video width="640" height="360" controls>\n<source src="${folderName}/${file}" type="video/mp4">\nYour browser does not support the video tag.\n</video>\n`;
-                }
-            });
-
-            // Close the HTML body and document
-            htmlContent += `\n</body></html>`;
-
-            // Save the HTML content to a file in a folder (e.g., "generated")
-            fs.writeFileSync('./'+mediaFoldersFolder+'/'+folderName+'.html', htmlContent);
-
-            return("<iframe src = "+mediaFoldersFolder+"/"+folderName+".html width = 100% height = 100% frameborder=0 style=border: none draggable=true></iframe>")
-
-
-
-
-            // gotta return something like this 
-            // "<iframe src = testiframe.html width = 100% height = 100% frameborder=0 style=border: none draggable=true></iframe>",
+                // Close the HTML body and document
+                htmlContent += `\n</body></html>`;
+                fs.writeFileSync('./'+mediaFoldersFolder+'/'+folderName+'.html', htmlContent);
+                return("<iframe src = "+mediaFoldersFolder+"/"+folderName+".html width = 100% height = 100% frameborder=0 style=border: none draggable=true></iframe>")
+            }
+        } catch (error) {
+            console.error(`Error reading directory: ${error}`);
         }
-
-    } catch (error) {
-        console.error(`Error reading directory: ${error}`);
-
     }
 }
 
+// Function to delete previously made html files
 function deleteHTMLFilesInFolder(folderName) {
     fs.readdirSync(folderName).forEach(file => {
         const filePath = path.join(folderName, file);
         if (path.extname(file) === '.html') {
             fs.unlinkSync(filePath);
-            console.log(`Deleted file: ${file}`);
         }
     });
 }
 
-
+// JS for main server operations
 app.get('/data', async (req, res) => {
-
-    //create the json as a js object
-    console.log('creating json from excel');
-
 
     try {
         const workbook = new ExcelJS.Workbook();
         await workbook.xlsx.readFile(excelFile);
         const worksheet = workbook.getWorksheet(1);
 
-        //delete old html files from mediaFoldersFolder
         deleteHTMLFilesInFolder(mediaFoldersFolder);
-
-        
-    
 
         var timelineJS_object = {};
 
-
         worksheet.eachRow((row, rowNum) => {
-
-
-            if (rowNum == 2){
-                // for title page
+            if (rowNum == 2){ // for title page
                 const rowData = {};
                 row.eachCell((cell, colNum) => {
                     rowData[worksheet.getRow(1).getCell(colNum).value] = cell.value;
@@ -151,15 +118,13 @@ app.get('/data', async (req, res) => {
                     },
                     events: []
                 };
-            }
-            if (rowNum > 2){ //skip the header row
+            } else if (rowNum > 2){ //skip the header row
                 const rowData = {};
                 row.eachCell((cell, colNum) => {
                     rowData[worksheet.getRow(1).getCell(colNum).value] = cell.value;
                 });
 
                 timelineJS_object.events.push({
-
                     ...(rowData['Media'] ? {
                         "media": 
                             {
@@ -168,16 +133,6 @@ app.get('/data', async (req, res) => {
                             "credit": rowData['Media_Credit']
                             }
                     } : {})
-
-                    /* FOR MULTIMEDIA
-                    "media": 
-                            {
-                            "url": "<iframe src = testiframe.html width = 100% height = 100% frameborder=0 style=border: none draggable=true></iframe>",
-                            "caption": rowData['Media_Caption'],
-                            "credit": rowData['Media_Credit']
-                            }
-                    */
-                    
                     ,
                     "start_date": {
                         "month": rowData['Month'],
@@ -205,14 +160,11 @@ app.get('/data', async (req, res) => {
             }
         });
 
-        // remove empty fields
-        
         const cleanedTimelineJSObject = removeEmptyFields(timelineJS_object);
-        // console.log(JSON.stringify(timelineJS_object));
 
-        console.log('JSON creation completed');
         const responsiveData = cleanedTimelineJSObject;
         res.json(responsiveData);
+
     } catch (error) {
         console.error('Error reading Excel file:',  error);
         res.status(500).json({ error: 'Error reading Excel file'});
@@ -222,6 +174,8 @@ app.get('/data', async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
+  console.log('Press F11 to enter fullscreen');
+  console.log('ctrl+C to manually terminate server\n');
 });
 
 app.post('/notify-close', (req, res) => {
@@ -232,6 +186,9 @@ app.post('/notify-close', (req, res) => {
         console.log('Server has been shut down');
         process.exit(0);
     });
+
+    //clean up html files
+    deleteHTMLFilesInFolder();
 
     //Server side shutdown
     res.status(200).send('Recieved notifiation');
